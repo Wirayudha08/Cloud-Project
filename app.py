1df4cbcb26640b09c9da7839ce30658edabe22d0
Wirayudha08 <rachmadwirayudha08@gmail.com>
2025-07-07 16:47:12 +0700

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -22,5 +23,6 @@ def contact():
         print("Error:", str(e), flush=True)
         return "Terjadi kesalahan pada server.", 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
